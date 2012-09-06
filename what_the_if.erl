f5bf829f878b9caa10c087528a78c707a2a03966
Ahmed Ezzat Afifi <AEzzatA@gmail.com>
2012-09-06 12:48:04 +0200

    What the if

---
+++ what_the_if.erl
@@ -0,0 +1,35 @@
+-module(what_the_if).
+-export([heh_fine/0, oh_god/1,help_me/1, insert/2]).
+
+heh_fine() ->
+	if 1 =:= 1 ->
+		works
+	end,
+	if 1 =:=2; 1 =:= 1 ->
+		works
+	end,
+	if 1=:= 2, 1 =:= 1 ->
+		fails
+	end.
+
+oh_god(N) ->
+	if N =:= 2 -> might_succeed;
+		true -> always_does
+	end.
+help_me(Animal) ->
+	Talk = if 
+		Animal == cat -> "meow";
+		Animal == beef -> "mooo";
+		Animal == dog -> "bark";
+		Animal == tree -> "bark";
+		true -> "fgdadfgna"
+	end,
+	{Animal, "Says " ++ Talk ++ "!"}.
+
+insert(X,[]) ->
+	[X];
+insert(X,Set) ->
+	case lists:member(X,Set) of
+		true -> Set;
+		false -> [X|Set]
+	end.
